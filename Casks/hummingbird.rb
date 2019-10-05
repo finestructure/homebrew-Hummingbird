12938aea2c96ba1b7ac4e861553d175baa94f2a2
Sven A. Schmidt <sas@finestructure.co>
2019-10-05 12:45:10 +0200

    Release 3.0.0-12

--- Casks/hummingbird.rb
+++ Casks/hummingbird.rb
@@ -1,6 +1,6 @@
 cask 'hummingbird' do
-  version '3.0.0-11'
-  sha256 '45b9b62eef4819d58dd5b730d358e24e91dc1bed38efe0c8a145df7db358cd08'
+  version '3.0.0-12'
+  sha256 'd2798b34d333d86d0a928f6242dfc89480c54ab4da4f7c9f597ff0b86b5d84ba'
 
   url "https://finestructure.co/s/Hummingbird-#{version.no_dots}.zip"
   name 'Hummingbird'
